58f8631059e93c4cdd1580012c35701b6ef79d01
Suprasiddha Maharjan <suppu@Suprasiddhas-MacBook-Air.local>
2025-12-11 18:48:37 +0545

    server.js finalize

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
-import express from 'express';
-import { configDotenv } from 'dotenv';
+import express from "express";
+import { configDotenv } from "dotenv";
+import bodyParser from "body-parser";
+import routes from "./RouteServices";
 
 const config = configDotenv();
 
-const app = express()
+const app = express();
 
-const port = process.env.DB_PORT;
+const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-})
+  console.log(`App running on port ${port}`);
+});
+
+app.use(bodyParser);
+app.use("/grande-realtors/api", routes);
